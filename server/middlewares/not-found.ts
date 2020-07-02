0105a88bbcddad9f53ccb08c7d1d392b90cae427
romain@info80.fr <romain@info80.fr>
2020-07-02 17:15:01 +0200

    CLEAN

--- server/middlewares/not-found.ts
+++
@@ -1,4 +0,0 @@
-export default ({ response }: any) => {
-    response.status = 404;
-    response.body = { msg: "Not Found" };
-};
--- server/routing.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
-import handleChat from "./handlers/chat.ts";
-
-const routing = new Router();
-
-routing
-    .get("/ws", handleChat);
-
-export default routing;
